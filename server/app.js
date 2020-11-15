6176210db57ff9616f4b329ac3df930cb0ae9f27
TJ (qedpi) <junruihu@gmail.com>
2020-11-14 19:02:28 -0500

    prepare for heroku

--- server/app.js
+++ server/app.js
@@ -1,14 +1,16 @@
 const http = require('http');
+const express = require('express');
+const path = require('path');
+
+const cool = require('cool-ascii-faces');
 
 const hostname = '127.0.0.1';
-const port = 3000;
+const PORT = process.env.PORT || 5000;
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Test Hello Test World');
-});
-
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-});
+express()
+    .use(express.static(path.join(__dirname, 'public')))
+    .set('views', path.join(__dirname, 'views'))
+    .set('view engine', 'ejs')
+    // .get('/cool', (req, res) => res.render('pages/index'))
+    .get('/', (req, res) => res.send(cool()))
+    .listen(PORT, () => console.log(`Listening on ${ PORT }`));
