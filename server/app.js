73c610e3f277077a9f79a4ade0bb8162f44f5776
TJ (qedpi) <junruihu@gmail.com>
2020-11-15 08:15:55 -0500

    remove procfile

--- server/app.js
+++ server/app.js
@@ -1,10 +1,7 @@
-const http = require('http');
 const express = require('express');
 const path = require('path');
-
 const cool = require('cool-ascii-faces');
 
-const hostname = '127.0.0.1';
 const PORT = process.env.PORT || 5000;
 
 express()
