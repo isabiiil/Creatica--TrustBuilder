fb77dc65cb0fa4b90d8771647d39a87d605317d6
Corinne Kadri <68750694+corinnekadri@users.noreply.github.com>
2020-11-15 13:21:39 -0500

    Update Navbar.js

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -14,7 +14,7 @@ class Navbar extends Component {
     render() {
         return(
             <nav className="NavbarItems">
-                <h1 className="navbar-logo">Group Name<i className="fab fa-react"></i></h1>
+                <h1 className="navbar-logo">Trust Builder<i className="fab fa-react"></i></h1>
                 <div className="menu-icon" onClick={this.handleClick}>
                     <i className={this.state.clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
                 </div>
